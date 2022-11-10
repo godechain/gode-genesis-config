4774080b2ed265d34323cc4645fa20ed174ee11c
godechain <103926687+godechain@users.noreply.github.com>
2022-11-10 17:14:54 +0800

    genesis contract

---
+++ test/injector.js
@@ -0,0 +1,15 @@
+const {newMockContract} = require("./helper");
+
+contract("Injector", async (accounts) => {
+  const [owner] = accounts
+  it("migration is working fine", async () => {
+    const {staking, slashingIndicator, systemReward, governance, chainConfig} = await newMockContract(owner);
+    for (const contract of [staking]) {
+      assert.equal(staking.address, await contract.getStaking());
+      assert.equal(slashingIndicator.address, await contract.getSlashingIndicator());
+      assert.equal(systemReward.address, await contract.getSystemReward());
+      assert.equal(governance.address, await contract.getGovernance());
+      assert.equal(chainConfig.address, await contract.getChainConfig());
+    }
+  });
+});
---
+++ test/runtime-upgrade.js
@@ -0,0 +1,16 @@
+const FakeRuntimeUpgradeEvmHook = artifacts.require('FakeRuntimeUpgradeEvmHook');
+const {newMockContract} = require("./helper");
+
+contract("RuntimeUpgrade", async (accounts) => {
+  const [owner] = accounts
+  it("migration is working fine", async () => {
+    const evmHook = await FakeRuntimeUpgradeEvmHook.new();
+    const {runtimeUpgrade} = await newMockContract(owner, {runtimeUpgradeEvmHook: evmHook.address});
+    assert.equal(await runtimeUpgrade.getEvmHookAddress(), evmHook.address);
+    const systemSmartContracts = await runtimeUpgrade.getSystemContracts();
+    const res = await runtimeUpgrade.upgradeSystemSmartContract(systemSmartContracts[0], '0xbadcab1e', '0x');
+    assert.equal(res.logs[0].event, 'SmartContractUpgrade')
+    assert.equal(res.logs[0].args.contractAddress, systemSmartContracts[0])
+    assert.equal(res.logs[0].args.newByteCode, '0xbadcab1e')
+  });
+});
